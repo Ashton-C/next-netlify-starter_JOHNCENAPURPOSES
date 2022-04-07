d268783a394eee3f9caee0092d23879d4e342f53
Ashton Christensen <armadillomaster@gmail.com>
2022-04-06 23:04:02 -0700

    Cena time fr fr

--- pages/index.js
+++ pages/index.js
@@ -11,8 +11,9 @@ export default function Home() {
       </Head>
 
       <main>
-        <iframe width="560" height="315" src="https://www.youtube.com/embed/XgUB3lF9IQA" start="0" end="6" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      </main>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/XgUB3lF9IQA?rel=0?version=3&autoplay=1&start=0&end=6&loop=1&controls=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; loop; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <p> Get Pranked ya scallywag! </p>
+	</main>
 
     </div>
   )
