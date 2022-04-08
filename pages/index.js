137722fcadfc8121dc0805d6e3c412f109ac1c3f
Ashton Christensen <armadillomaster@gmail.com>
2022-04-07 22:13:26 -0700

    hotfix
    removed profanity

--- pages/index.js
+++ pages/index.js
@@ -6,7 +6,7 @@ export default function Home() {
   return (
     <div className="container">
       <Head>
-        <title>GET JOHN CENA'D FUCKER</title>
+        <title>GET JOHN CENA'D</title>
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
